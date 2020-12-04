f43041915a1d22489a55ef0bb1e358dd8900a454
thesimdak <thesimdak@gmail.com>
2020-12-04 19:59:23 +0100

    Update heroku config

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/svetsplhu-isos-app'));
+app.use(express.static('./dist/isos-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/svetsplhu-isos-app/index.html'));
+res.sendFile(path.join(__dirname,'/dist/isos-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
